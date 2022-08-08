13af747c2cfea7d4badcdcf3ac6f290b425fa8b8
seaSpiderSara <am9106@nyu.edu>
2022-08-08 00:51:58 -0400

    19

--- link to the past clone/Assets/sara_scripts/button_o_opens_door.cs
+++ link to the past clone/Assets/sara_scripts/button_o_opens_door.cs
@@ -8,12 +8,17 @@ public class button_o_opens_door : MonoBehaviour
     public Rigidbody2D this_door;
     public GameObject this_door_;
 
+    public Rigidbody2D pair_door;
+    public GameObject pair_door_;
+
     public Animator this_door_animator;
+    public Animator pair_door_animator;
 
     private void Start()
     {
         door_open = false;
         this_door_animator = this_door.GetComponent<Animator>();
+        pair_door_animator = pair_door.GetComponent<Animator>();
     }
     
     private void FixedUpdate()
@@ -21,6 +26,7 @@ public class button_o_opens_door : MonoBehaviour
         if(door_open == false)
         {
             this_door_animator.SetBool("locked", true);
+            pair_door_animator.SetBool("locked", true);
         }
     }
 
@@ -32,16 +38,27 @@ public class button_o_opens_door : MonoBehaviour
             if(door_open == false)
             {
                 door_open = true;
+                //play animations
                 this_door_animator.SetBool("locked", false);
+                pair_door_animator.SetBool("locked", false);
+                //no more collision, Link can go through
                 this_door.GetComponent<BoxCollider2D>().enabled = false;
+                pair_door.GetComponent<BoxCollider2D>().enabled = false;
+                //doors disappears after delay
                 StartCoroutine(Timedelay());
             } 
             else if(door_open == true)
             {
                 door_open = false;
+                //animation
                 this_door_animator.SetBool("locked", true);
+                pair_door_animator.SetBool("locked", true);
+                //colliders back, Lick cant go
                 this_door.GetComponent<BoxCollider2D>().enabled = true;
+                pair_door.GetComponent<BoxCollider2D>().enabled = true;
+                //door can be seen again
                 this_door.GetComponent<SpriteRenderer>().enabled = true;
+                pair_door.GetComponent<SpriteRenderer>().enabled = true;
             }
         }
     }
@@ -51,6 +68,7 @@ public class button_o_opens_door : MonoBehaviour
     {
         yield return new WaitForSeconds(0.3f);
         this_door.GetComponent<SpriteRenderer>().enabled = false;
+        pair_door.GetComponent<SpriteRenderer>().enabled = false;
     }
 
 }
